c97cd58df29ecde664c3b1e7305ef1aef2a3cd3b
Diogo Marques <xmarques999@hotmail.com>
2016-03-01 23:06:15 +0000

    bandeiras e respostas :up:

--- template/footer.php
+++ template/footer.php
@@ -1,1 +1,19 @@
-<script>$(function(){footerLinks=$("ul#footer-links"),footerElem=$("#footer"),footerLinks.hide(),footerElem.hover(function(){footerLinks.stop(!0,!0).slideToggle("fast")})});</script><footer id="footer" class="clearfix"><div class="ink-grid fw-regular"><ul id="footer-links" class="unstyled small inline quarter-vertical-space"><li><a href="about.php">About</a></li><li><a href="sitemap.php">Sitemap</a></li><li><a href="contacts.php">Contacts</a></li></ul><p class="note medium no-margin half-vertical-padding"><i class="fa fa-copyright"></i> 2015 Carlos Samouco, Diana Pinto, Diogo Marques</p></div></footer></body></html>
+<script>
+    $(function() {
+        footerLinks = $("ul#footer-links"), footerElem = $("#footer"), footerLinks.hide(), footerElem.hover(function() {
+            footerLinks.stop(true, true).slideToggle("fast")
+        })
+    });
+</script>
+<footer id="footer" class="clearfix">
+    <div class="ink-grid fw-regular">
+        <ul id="footer-links" class="unstyled small inline quarter-vertical-space">
+            <li><a href="about.php">About</a></li>
+            <li><a href="sitemap.php">Sitemap</a></li>
+            <li><a href="contacts.php">Contacts</a></li>
+        </ul>
+        <p class="note medium no-margin half-vertical-padding"><i class="fa fa-copyright"></i> 2015 Diogo Marques, Pedro Melo, Vitor Esteves</p>
+    </div>
+</footer>
+</body>
+</html>
