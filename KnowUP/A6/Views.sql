a54d64bf2c5d6003dc8b70a1f81cb7bb27c9c21e
Diogo Marques <xmarques999@hotmail.com>
2016-04-02 15:31:02 +0100

    merge das views :peach:

--- KnowUP/A6/Views.sql
+++ KnowUP/A6/Views.sql
@@ -18,3 +18,49 @@ CREATE OR REPLACE VIEW knowUP.MembrosInstituicao AS
 	FROM knowUP.Instituicao
 	LEFT JOIN knowUP.Utilizador USING(idInstituicao)
 	GROUP BY Instituicao.idInstituicao;
+	
+/*--------------------------------------------*/
+/* 1. LISTAS DE PERGUNTAS POR CATEGORIA      */
+/*--------------------------------------------*/
+DROP VIEW IF EXISTS knowUP.PerguntasPorCategoria;
+CREATE VIEW knowUP.PerguntasPorCategoria AS
+SELECT Perguntas.idCategoria, Perguntas.texto, Categorias.nome
+   FROM knowUP.Perguntas
+   JOIN knowUP.Categorias ON Perguntas.idCategoria = Categorias.idCategoria
+   ORDER BY Perguntas.idCategoria;
+   
+/*--------------------------------------------*/
+/* 1. LISTAS DE RESPOSTAS A UMA PERGUNTA      */
+/*--------------------------------------------*/
+DROP VIEW IF EXISTS knowUP.RespostasPergunta;
+CREATE VIEW knowUP.RespostasPergunta AS
+SELECT Respostas.idResposta, Utilizadores.username, Respostas.titulo, Respostas.descricao
+	FROM knowUP.Respostas, knowUP.Perguntas, knowUP.Utilizadores
+	WHERE Perguntas.idPergunta = resposta.idPergunta
+	AND Respostas.idAutor = Utilizadores.idUtilizador
+	ORDER BY Perguntas.timestamp;
+	
+/*--------------------------------------------*/
+/* LISTA DAS PERGUNTA MAIS POPULARES          */
+/*--------------------------------------------*/
+DROP VIEW IF EXISTS knowUP.PerguntasPopulares;
+CREATE VIEW knowUP.PerguntasPopulares AS
+SELECT Perguntas.idPergunta, Perguntas.titulo, Perguntas.descricao, (contribuicao.votospositivos + contribuicao.votosnegativos) as pontuacao
+	FROM knowUP.Perguntas, knowUP.Utilizadores
+	WHERE pergunta.perguntaid = contribuicao.contribuicaoid
+	ORDER BY contribuicao.votospositivos + contribuicao.votosnegativos DESC;
+	
+DROP VIEW IF EXISTS knowUP.RespostasMembro;
+CREATE VIEW knowUP.RespostasMembro AS
+SELECT resposta.correcta, resposta.descricao
+   FROM knowUP.Utilizadores
+   JOIN knowUP.Respostas ON Utilizadores.idUtilizador = Respostas.idAutor;
+   
+/*--------------------------------------------
+/* LISTA DAS PERGUNTAS MAIS VISTAS
+/*--------------------------------------------*/
+DROP VIEW IF EXISTS knowUP.PerguntasMaisVistas;
+CREATE VIEW knowUP.PerguntasMaisVistas AS
+SELECT Perguntas.idPergunta, Perguntas.titulo, Perguntas.descricao, Perguntas.visualizacoes
+	FROM knowUP.Perguntas
+	ORDER BY Perguntas.visualizacoes;
