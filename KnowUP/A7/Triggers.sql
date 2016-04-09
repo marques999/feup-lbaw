39385af726e1219547cc8038d9c4babee23000e4
Diogo Marques <xmarques999@hotmail.com>
2016-04-10 00:35:05 +0100

    alguns gatilhos :triumph:

--- KnowUP/A7/Triggers.sql
+++ KnowUP/A7/Triggers.sql
@@ -1,35 +1,146 @@
-CREATE FUNCTION apagarVoto() RETURNS trigger AS $decrementVotes$
+CREATE OR REPLACE FUNCTION knowUP.classificarPergunta() RETURNS trigger AS $classificarPergunta$
+DECLARE
+	AutorPergunta integer;
+	AutorVoto integer;
+BEGIN
+	AutorVoto = NEW.idAutor;
+	SELECT Pergunta.idAutor INTO AutorPergunta
+	FROM Pergunta WHERE NEW.idPergunta = Pergunta.idPergunta;
+	IF (AutorVoto == AutorPergunta) THEN
+		RETURN NULL;
+	END IF;
+	RETURN NEW;	
+END;
+
+$classificarPergunta$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION knowUP.classificarResposta() RETURNS TRIGGER AS $classificarResposta$
+DECLARE
+	AutorResposta integer;
+	AutorVoto integer;
+BEGIN
+	AutorVoto = NEW.idAutor;
+	SELECT Contribuicao.idAutor INTO AutorResposta
+	FROM Resposta
+	JOIN Contribuicao WHERE Contribuicao.idContribuicao = Resposta.idResposta;
+	IF (AutorVoto == AutorResposta) THEN
+		RETURN NULL;
+	END IF;
+	RETURN NEW;
+END;
+
+$classificarResposta$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION knowUP.responderPergunta() RETURNS TRIGGER as $responderPergunta$
+DECLARE
+	AutorPergunta integer;
+	AutorResposta integer;
+BEGIN
+	SELECT Pergunta.idAutor INTO AutorPergunta
+	FROM Pergunta WHERE NEW.idPergunta = Pergunta.idPergunta;
+	SELECT Contribuicao.idAutor INTO AutorResposta
+	FROM Contribuicao WHERE NEW.idContribuicao = Contribuicao.idContribuicao;
+	IF (AutorResposta == AutorPergunta) THEN
+		RETURN NULL;
+	END IF;
+	RETURN NEW;
+END;
+
+$responderPergunta$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION knowUP.melhorResposta() RETURNS trigger AS $melhorResposta$
+BEGIN
+	IF EXISTS (SELECT * FROM knowUP.Pergunta WHERE knowUP.Pergunta.melhorResposta) THEN
+		RETURN NULL;
+	END IF;
+	RETURN NEW;
+END;
+
+$melhorResposta$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION knowUP.registarVotoPergunta(integer, integer, integer)
+RETURNS void AS $registarVotoPergunta$
+BEGIN
+    IF EXISTS(SELECT 1 FROM knowUP.VotoPergunta WHERE idPergunta = $1 AND idAutor = $2) THEN
+        UPDATE knowUP.VotoPergunta SET valor = $3 WHERE idPergunta = $1 AND idAutor = $2);
+        RETURN;
+    ELSE
+        INSERT INTO knowUP.VotoPergunta VALUES ($1, $2, $3);
+        RETURN;
+    END IF;
+END;
+
+$registarVotoPergunta$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION knowUP.registarVotoPergunta(integer, integer, integer)
+RETURNS void AS $registerVotoResposta$
+BEGIN
+	IF EXISTS(SELECT 1 FROM knowUP.VotoResposta WHERE idResposta = $1 AND idAutor = $2) THEN
+		UPDATE knowUP.VotoResposta SET valor = $3 WHERE idResposta = $1 AND idAutor = $2);
+		RETURN;
+	ELSE
+		INSERT INTO knowUP.VotoResposta VALUES ($1, $2, $3);
+		RETURN;
+	END IF;
+END;
+
+$registarVotoResposta$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION calcularPontuacaoPergunta(integer) RETURNS integer AS $calcularPontuacaoPergunta$
+BEGIN
+	SELECT SUM(VotoPergunta.valor) AS pontuacao FROM VotoPergunta
+	WHERE VotoPergunta.idPergunta = $1;
+END;
+
+$calcularPontuacaoPergunta$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION calcularPontuacaoResposat(integer) RETURNS integer AS $calcularPontuacaoResposat$
+BEGIN
+	SELECT SUM(VotoResposta.valor) AS pontuacao FROM VotoResposta
+	WHERE VotoResposta.idResposta = $;
+END;
+
+$calcularPontuacaoResposta$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION knowUP.formatarData(timestamp) RETURNS text AS $formatarData$
+BEGIN
+	SELECT to_char($1, 'Day, DD/MM/SS HH:MI');
+END;
+
+$formatarData$ LANGUAGE plpgsql;
+
+CREATE TRIGGER TRIGGER_seguirPergunta
+	AFTER INSERT ON knowUP.Pergunta
+	FOR EACH ROW
 	BEGIN
-		IF new.valor = 1
-			THEN UPDATE Perguntas SET
-				votosPositivos = votosPositivos - 1,
-				pontuacao = pontuacao - 1
-			WHERE new.contribuicaoID = contribuicaoID;
-		ELSE IF new.valor = -1
-			THEN UPDATE Perguntas SET
-				votosNegativos = votosNegativos - 1,
-				pontuacao = pontuacao + 1
-			WHERE new.contribuicaoID = contribuicaoID;
-		END IF;
-		return new;
+		INSERT INTO Seguidor(NEW.idAutor, NEW.idPergunta, now(), now());
 	END;
 
-$apagarVoto$ LANGUAGE plpgsql;
+CREATE TRIGGER TRIGGER_classificarPergunta
+	BEFORE INSERT ON knowUP.VotoPergunta
+	FOR EACH ROW
+	EXECUTE PROCEDURE knowUP.FUNC_classificarPergunta();
 
-CREATE TRIGGER incrementarVotosPergunta
-	AFTER INSERT ON VotoPergunta
+CREATE TRIGGER TRIGGER_classificarResposta
+	BEFORE INSERT ON knowUP.VotoResposta
 	FOR EACH ROW
-	EXECUTE PROCEDURE knowUP.incrementVotes();
-CREATE TRIGGER incrementarVotosResposta
-	AFTER INSERT ON VotoResposta
-	FOR EACH ROW
-	EXECUTE PROCEDURE knowUP.incrementVotes();
+	EXECUTE PROCEDURE knowUP.FUNC_classificarResposta();
 
-CREATE TRIGGER decrementarVotosPergunta
-	BEFORE DELETE ON knowUP.VotosPergunta
+CREATE TRIGGER TRIGGER_responderPergunta
+	BEFORE INSERT ON knowUP.Resposta
 	FOR EACH ROW
-	EXECUTE PROCEDURE knowUP.decrementVotes();
-CREATE TRIGGER decrementarVotosResposta
-	BEFORE DELETE ON knowUP.VotosResposta
+	EXECUTE PROCEDURE knowUP.FUNC_responderPergunta();
+
+CREATE TRIGGER TRIGGER_melhorResposta
+	BEFORE INSERT OR UPDATE ON knowUP.Resposta
 	FOR EACH ROW
-	EXECUTE PROCEDURE knowUP.decrementVotes();
+	WHEN (NEW.melhorResposta)
+	EXECUTE PROCEDURE knowUP.FUNC_melhorResposta();
+
+CREATE TRIGGER TRIGGER_alterarVotoPergunta
+	BEFORE INSERT ON knowUP.VotoPergunta
+	FOR EACH ROW
+	WHEN EXISTS (SELECT * FROM VotoPergunta
+		WHERE VotoPergunta.idPergunta == NEW.idPergunta
+		AND VotoPergunta.idAutor == NEW.idAutor)
+	EXECUTE PROCEDURE knowUP.FUNC_alterarVoto();
--- KnowUP/A7/Views.sql
+++ KnowUP/A7/Views.sql
@@ -1,22 +1,28 @@
 DROP VIEW IF EXISTS knowUP.CategoriasPorInstituicao;
 CREATE VIEW knowUP.CategoriasPorInstituicao AS
-	SELECT Instituicao.sigla, Categoria.idCategoria, Categoria.nome
+	SELECT  Instituicao.sigla,
+			Categoria.idCategoria,
+			Categoria.nome
 	FROM knowup.Instituicao
 	JOIN knowup.CategoriaInstituicao USING (idInstituicao)
 	JOIN knowup.Categoria USING (idCategoria)
 	ORDER BY Instituicao.sigla, Categoria.nome;
 
-DROP VIEW IF EXISTS knowUP.MembrosInstituicao;
+DROP VIEW IF EXISTS knowUP.VistaInstituicao;
 CREATE OR REPLACE VIEW knowUP.MembrosInstituicao AS
 	SELECT Instituicao.idInstituicao,
-		Instituicao.nome,
-		Instituicao.sigla,
-		Instituicao.morada,
-		Instituicao.contacto,
-		Instituicao.website,
-		COALESCE(count(Utilizador.idUtilizador), 0) AS count
+		   Instituicao.nome,
+	       Instituicao.sigla,
+		   Instituicao.morada,
+		   Instituicao.contacto,
+		   Instituicao.website,
+		   COALESCE(count(Utilizador.idUtilizador), 0) AS numeroUtilizadores,
+		   COALESCE(count(Pergunta.idPergunta), 0) AS numeroPerguntas,
+		   COALESCE(sum(Pergunta.visualizacoes), 0) AS numeroVisualizacoes
 	FROM knowUP.Instituicao
-	LEFT JOIN knowUP.Utilizador USING(idInstituicao)
+	LEFT JOIN knowUP.Utilizador USING(idInstituicao),
+	LEFT JOIN knowUP.CategoriaInstituicao USING (idInstituicao),
+	LEFT JOIN knowUP.Pergunta USING (idCategoria),
 	GROUP BY Instituicao.idInstituicao;
 	
 /*--------------------------------------------*/
@@ -32,16 +38,16 @@ SELECT Perguntas.idCategoria, Perguntas.texto, Categorias.nome
 /*--------------------------------------------*/
 /* 1. LISTAS DE RESPOSTAS A UMA PERGUNTA      */
 /*--------------------------------------------*/
-DROP VIEW IF EXISTS knowUP.RespostasPergunta;
-CREATE VIEW knowUP.RespostasPergunta AS
-SELECT Respostas.idResposta, Utilizadores.username, Respostas.titulo, Respostas.descricao
+DROP VIEW IF EXISTS knowUP.NumeroRespostas;
+CREATE VIEW knowUP.NumeroRespostas AS
+SELECT Pergunta.idPergunta, Utilizadores.username, Respostas.titulo, Respostas.descricao
 	FROM knowUP.Respostas, knowUP.Perguntas, knowUP.Utilizadores
 	WHERE Perguntas.idPergunta = resposta.idPergunta
 	AND Respostas.idAutor = Utilizadores.idUtilizador
 	ORDER BY Perguntas.timestamp;
 	
 /*--------------------------------------------*/
-/* LISTA DAS PERGUNTA MAIS POPULARES          */
+/* LISTA DAS PERGUNTAS MAIS POPULARES          */
 /*--------------------------------------------*/
 DROP VIEW IF EXISTS knowUP.PerguntasPopulares;
 CREATE VIEW knowUP.PerguntasPopulares AS
@@ -52,9 +58,18 @@ SELECT Perguntas.idPergunta, Perguntas.titulo, Perguntas.descricao, (contribuica
 	
 DROP VIEW IF EXISTS knowUP.RespostasMembro;
 CREATE VIEW knowUP.RespostasMembro AS
-SELECT resposta.correcta, resposta.descricao
-   FROM knowUP.Utilizadores
-   JOIN knowUP.Respostas ON Utilizadores.idUtilizador = Respostas.idAutor;
+SELECT Resposta.descricao, Resposta.dataHora, Resposta.melhorResposta
+   FROM knowUP.Utilizador
+   JOIN knowUP.Resposta ON Utilizadores.idUtilizador = Respostas.idAutor
+   WHERE Utilizadores.idUtilizador = :idUtilizador;
+
+DROP VIEW IF EXISTS PerguntasMembro;
+CREATE VIEW PerguntasMembro AS
+SELECT Pergunta.titulo, Pergunta.descricao, Pergunta.dataHora
+	FROM Utilizador
+	JOIN Pergunta ON Utilizador.idUtilizador = Pergunta.idAutor
+	WHERE Utilizador.idUtilizador = :idUtilizador;
+
    
 /*--------------------------------------------
 /* LISTA DAS PERGUNTAS MAIS VISTAS
